b0463fbff05e347becc074a51654f23da1bf7427
Satyam_R <satyamrahangdale196@gmail.com>
2024-10-04 07:19:35 +0530

    made changes for depolyment on github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PromptAdvisors_remastered/',
 })
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Weather_App/', // Replace with your GitHub repository name
+// });
